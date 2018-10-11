03d0cd52ca25e2972a04f0976ec32de0d53d8c8f
Hadrien <hadrien.lorenzo.2015@gmail.com>
2018-10-11 09:49:22 +0200

    debug + 1

--- R/plot.perf_mddsPLS.R
+++ R/plot.perf_mddsPLS.R
@@ -37,7 +37,11 @@ plot.perf_mddsPLS <- function(x,plot_mean=FALSE,legend_names=NULL,
 
   X_all <- scale(do.call(cbind,res_perf_mdd$Xs))
   if(res_perf_mdd$mode=="reg"){
-    cc <- t(na.omit(abs(crossprod(X_all,scale(res_perf_mdd$Y))/(nrow(res_perf_mdd$Y)-1))))
+    cc <- abs(crossprod(scale(res_perf_mdd$Y),X_all)/(nrow(res_perf_mdd$Y)-1))
+    col_na <- which(is.na(colSums(cc)))
+    if(length(col_na)>0){
+      cc <- cc[,-col_na,drop=F]
+    }
   }
   else{
     Y_df <- data.frame(res_perf_mdd$Y)
